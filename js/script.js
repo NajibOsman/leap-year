3bdcd9bdb91b760056abf07c9fd5bb8e1a8472e0
NajibOsman <machidydemajka38@gmail.com>
2017-02-22 10:09:02 +0300

    updated the scripts.js with JQeury

--- js/script.js
+++ js/script.js
@@ -10,3 +10,10 @@ $(document).ready(function() {
   var leapYear = function(year) {
       return false;
     };
+    var leapYear = function(year) {
+        if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+          return true;
+        } else {
+          return false;
+        }
+      };
